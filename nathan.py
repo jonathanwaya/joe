dc7b16dfdd1b88c459ccb29da523fe9490534994
jonathan waya <nathanzw00@gmail.com>
2025-09-02 19:40:29 +0200

    Add files via upload

---
+++ nathan.py
@@ -0,0 +1,13 @@
+# Define a constant (by convention, constants are written in uppercase)
+PI = 3.14159
+
+# Define variables
+radius = 5  # Radius of a circle
+area = PI * (radius ** 2)  # Calculate the area of the circle
+
+
+print("The radius of the circle is:", radius)
+print("The area of the circle is:", area)
+
+
+
